c70ce7af275ae4321a6e78bd1220ce50f14672de
Aditya <adi247u@gmail.com>
2024-09-12 14:29:26 +0530

    Spell Correct

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,7 +6,7 @@ const Landing = () => {
         <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 items-center w-full">
             <div className="flex flex-col space-y-4 text-center md:text-left">
                 <div className="text-5xl font-bold">Detect Malaria with AI</div>
-                <div className="text-3xl font-semibold text-[#2463eb]">Prevent Malria Outbreaks</div>
+                <div className="text-3xl font-semibold text-[#2463eb]">Prevent Malaria Outbreaks</div>
                 <div className="text-[#4b5563]">Our cutting-edge AI technology analyzes blood smear images to quickly and accurately detect malaria parasites. By providing early detection, we help prevent outbreaks and save lives in at-risk communities.</div>
             </div>
             <Lottie animationData={Art} loop={true}/>
