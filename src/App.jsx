34faffbbff226a1eb8e32197bbf7450392d9ca27
Aditya <adi247u@gmail.com>
2024-09-12 14:25:48 +0530

    Landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Landing from "./pages/Landing";
 
 export default function App() {
-  return(
-    <div className="">
+  return (
+    <div className="bg-gradient-to-b from-blue-100 to-white md:px-32 p-16">
+    <Landing/>
     </div>
   )
 }
