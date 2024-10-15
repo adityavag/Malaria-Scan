1bb2b455265883f7ab556940dbab7bf5253e65a1
Aditya <adi247u@gmail.com>
2024-10-15 14:58:32 +0530

    feat: Flask API Integrated

--- src/pages/Detection.jsx
+++ src/pages/Detection.jsx
@@ -3,17 +3,42 @@ import Upload from "../assets/lottie/Upload.json";
 import Scan from "../assets/lottie/Scan.json";
 import Lottie from "lottie-react";
 import { useTranslation } from "react-i18next";
-import { SquareScissors, UploadIcon } from "lucide-react";
+import { Flag, SquareScissors, UploadIcon } from "lucide-react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
+import LoadingArt from "../assets/lottie/Loading.json";
 
 const Detection = () => {
   const [image, setImage] = useState(null);
   const [data, setData] = useState({});
   const { t } = useTranslation();
+  const [loading, setLoading] = useState(false);
 
-  const onPost = (e) => {
-    console.log("onPost Function Called");
-    // 
+  const onImageChange = (e) => {
+    setImage(e.target.files[0]);
+  }
+
+  const onPost = async (e) => {
+    setLoading(true)
+    const form_data = new FormData();
+    form_data.append("file", image);
+    axios.post('http://127.0.0.1:5000/predict', form_data, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            "Access-Control-Allow-Origin": "*",
+        },
+    }).then((result) => {
+      const a = result.data[0].prediction[0][0];
+      const b = result.data[0].prediction[0][1];
+      let answer;
+      a > b ? answer = "Infected" : "Uninfected";
+      setTimeout(() => {
+        setLoading(false); 
+        alert(answer);
+      },2000);
+    }).catch((err) => {
+      console.log(err);
+    })
   }
 
   const handleFileChange = (event) => {
@@ -22,54 +47,49 @@ const Detection = () => {
       setImage(selectedImage);
     }
   };
-
-  return (
-    <div className="">
-    <Navbar/>
-    <div className="mt-28 text-3xl font-bold text-center">Upload Blood Smear Slide Image</div>
-          <div className="grid grid-cols-1 mx-auto w-1/2 item-center justify-center min-h-screen mt-10 max-lg:w-full max-lg:px-2">
-      <div className="p-4 mx-6 flex justify-center items-center border-2 border-black rounded-md h-[500px]">
-        {image ? (
-          <img
-            src={URL.createObjectURL(image)}
-            alt="Selected"
-            className="max-h-full object-contain"
+  if(loading == false)
+  {
+    return (
+      <div className="">
+        <Navbar />
+        <div className="mt-28 text-3xl font-bold text-center">Upload Blood Smear Slide Image</div>
+        <div className="grid grid-cols-1 mx-auto w-1/2 item-center justify-center min-h-screen mt-10 max-lg:w-full max-lg:px-2">
+          <div className="p-4 mx-6 flex justify-center items-center border-2 border-black rounded-md h-[500px]">
+            {image ? (
+              <img
+                src={URL.createObjectURL(image)}
+                alt="Selected"
+                className="max-h-full object-contain"
+              />
+            ) : (
+              <Lottie animationData={Scan} loop={true} />
+            )}
+          </div>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+            className="file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 border-2 border-black rounded-md my-auto mx-auto p-2"
           />
-        ) : (
-          <Lottie animationData={Scan} loop={true} />
-        )}
-      </div>
-      <input
-          type="file"
-          accept="image/*"
-          onChange={handleFileChange}
-          className="file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 border-2 border-black rounded-md my-auto mx-auto p-2"
-        />
-        <button 
-          className="bg-blue-600 hover:bg-blue-700 text-white p-4 flex flex-row my-auto items-center justify-center space-x-2"
-          onClick={onPost}
+          <button
+            className="bg-blue-600 hover:bg-blue-700 text-white p-4 flex flex-row my-auto items-center justify-center space-x-2"
+            onClick={onPost}
           >
-        <UploadIcon/>
-        <span className="">Upload and Analyse</span>
-        </button>
-    </div>
-    </div>
-  );
+            <UploadIcon />
+            <span className="">Upload and Analyse</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+  else
+  {
+    return (
+      <div className="flex justify-center h-screen items-center">
+        <Lottie animationData={LoadingArt} loop={true} className="h-[100px] w-[100px]"/>
+      </div>
+    )
+  }
 };
 
 export default Detection;
-
-function FeatureCard({ title, description, icon }) {
-  return (
-    <div className="transition-all duration-300 hover:shadow-lg">
-      <div className="p-6">
-        <div className="flex justify-center mb-4">
-          {icon}
-        </div>
-        <h3 className="text-xl font-semibold text-gray-800 mb-2">{title}</h3>
-        <p className="text-gray-600">{description}</p>
-      </div>
-    </div>
-  )
-}
-{/* <FeatureCard title={"Malaria Presence Detected"} description={"h"} icon={<SquareScissors/>}/> */}
